369e5802b9126c014810deb87c12a7a95478a09b
loki <loki>
2021-12-15 16:15:49 +0500

    Добавил: -подписку по списку пользователей -сбор пользователей, прокомментировавших пост

--- main.py
+++ main.py
@@ -18,6 +18,7 @@ class InstagramBot:
     def login(self):
         browser = self.browser
         browser.get(self.link)
+        time.sleep(2)
 
         username_input = browser.find_element(By.NAME, 'username')
         username_input.clear()
@@ -43,17 +44,28 @@ class InstagramBot:
             exist = False
         return exist
 
+    # проверяет, подписан ли на пользователя
+    def should_be_subscribe(self):
+        browser = self.browser
+        try:
+            browser.find_element(By.XPATH, '//section/div[1]/div[1]/div/div[1]/button')
+            exist = False
+        except NoSuchElementException:
+            exist = True
+        return exist
+
     # отписка от всех
-    def unsubscribe_for_all_users(self, min_sleep=5, max_sleep=10, sleep_between_iterations=20, error_max=5):
+    def unsubscribe_for_all_users(self, min_sleep=1, max_sleep=5, sleep_between_iterations=20, error_max=5):
         browser = self.browser
         browser.get(f"https://www.instagram.com/{username}/")
 
         following_count = browser.find_element(
             By.XPATH, '/html/body/div[1]/div/div/section/main/div/header/section/ul/li[3]/a/span').text
         print(f"Количество подписок: {following_count}")
+        # счётчик перезапусков
+        error_count = 0
 
         while True:
-            error_count = 0
             if error_count >= error_max:
                 break
             try:
@@ -84,9 +96,18 @@ class InstagramBot:
             except NoSuchElementException:
                 error_count += 1
                 if error_count == error_max:
-                    print(f'----------- Элемент не найден, лимит перезапусков превышен, завершение. -----------')
+                    print(f'''
+                    -----------------------------------------------------------------------------------
+                    ----------- Элемент не найден, лимит перезапусков превышен, завершение. -----------
+                    -----------------------------------------------------------------------------------
+                           ''')
                 else:
-                    print(f'----------- Элемент не найден, перезапуск # {error_count}. -----------')
+                    print(f'''
+                    -----------------------------------------------------------------------------------
+                    ----------- Элемент не найден, перезапуск # {error_count}. ------------------------
+                    -----------------------------------------------------------------------------------
+                           ''')
+                time.sleep(20)
                 continue
 
     # ставит лайки по хэштегу
@@ -119,10 +140,66 @@ class InstagramBot:
                 print('----------- Лайк уже есть, переход к следующему посту. -----------')
                 continue
 
+    # собирает множестово из тех, кто комменировал пост
+    def select_commentators(self, link, number_scrolls=5, scrolls_timeout=1):
+        browser = self.browser
+        browser.get(link)
+        comments_ul = browser.find_element(By.XPATH, '//div[2]/div/div[2]/div[1]/ul')
+
+        for i in range(number_scrolls):
+            browser.execute_script("arguments[0].scrollTop = arguments[0].scrollHeight", comments_ul)
+            time.sleep(scrolls_timeout)
+
+            plus_button = browser.find_element(By.XPATH, '//div/div[2]/div[1]/ul/li/div/button')
+            plus_button.click()
+            time.sleep(scrolls_timeout)
+
+        comments_block = browser.find_element(By.XPATH, '//article/div/div[2]/div/div[2]/div[1]/ul')
+        user_comment_block = comments_block.find_elements(By.TAG_NAME, 'a')
+        users_urls = set()
+
+        for user_comment in user_comment_block:
+            user_url = user_comment.get_attribute('href')
+            len_user_url = len(user_url.split('/'))   # у ссылки на профиль пользователя это параметр равен пяти(5)
+            if len_user_url == 5:
+                users_urls.add(user_url)
+        print(f'Колличество собранных пользователей: {len(users_urls)}')
+        return users_urls
+
+    # подписыватеся на юзеров из списка, если нет списка, то вызывает "select_commentators"
+    def subscribe_to_user_list(self, user_list=None, timeout=40, scatter_timeout=10):
+        """
+        user_list - список юзеров для подписки
+        timeout - среднее время на одну подписку
+        scatter_timeout - разброс при вычислении таймаута
+        """
+        browser = self.browser
+        if user_list is None:
+            print('Списка нет, вызываю "select_commentators"')
+            user_list = self.select_commentators('https://www.instagram.com/p/CXfunm-Alrp/')
+            print('Список получен, перехожу к подписке.')
+
+        for user in user_list:
+            try:
+                browser.get(user)
+                assert self.should_be_subscribe()
+                time.sleep(random.randrange(timeout - scatter_timeout, timeout + scatter_timeout))
+                user_name = user.split("/")[-2]
+                subscribe_button = browser.find_element(By.XPATH, '//div/div/div/span/span[1]/button')
+                subscribe_button.click()
+                print(f'Подпислся на пользователя: {user_name}')
+            except AssertionError:
+                print('----------- Уже подписан, переход к следующему юзеру. -----------')
+                time.sleep(2)
+                continue
+
+
 
 my_bot = InstagramBot(username, password)
 try:
     my_bot.login()
-    my_bot.unsubscribe_for_all_users()
+    # my_bot.select_commentators('https://www.instagram.com/p/CXbevBlgtJF/')
+    my_bot.subscribe_to_user_list()
+    # my_bot.unsubscribe_for_all_users()
 finally:
     my_bot.close_browser()
